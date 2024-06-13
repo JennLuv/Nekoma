72ffc532a7b73748d6a93991636a975272ab5806
Jennifer <57284701+JennLuv@users.noreply.github.com>
2024-06-13 15:49:14 +0700

    added start view animation and startviewmodel

--- NekomaMap2/StartView.swift
+++ NekomaMap2/StartView.swift
@@ -10,23 +10,32 @@ import SwiftUI
 struct StartView: View {
     @Binding var isGameStarted: Bool
     @Binding var isLoading: Bool
+    @StateObject private var viewModel = StartViewModel()
     
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
-            Image("StartButton")
+            let currentImage = viewModel.animationFrames[viewModel.currentFrameIndex]
+            Image(currentImage)
                 .resizable()
                 .scaledToFit()
                 .frame(width: 300)
                 .onTapGesture {
                     isLoading = true
+                    viewModel.stopAnimation()
                     DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                         isGameStarted = true
                         isLoading = false
                     }
                 }
+                .onAppear {
+                    viewModel.startAnimation()
+                }
+                .onDisappear {
+                    viewModel.stopAnimation()
+                }
+            
         }
         .ignoresSafeArea()
     }
 }
-
---
+++ NekomaMap2/StartViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  StartViewModel.swift
+//  NekomaMap2
+//
+//  Created by Jennifer Luvindi on 13/06/24.
+//
+
+import SwiftUI
+import Combine
+
+class StartViewModel: ObservableObject {
+    @Published var currentFrameIndex = 0
+    private var timer: Timer?
+    
+    let animationFrames: [String] = (0..<2).map { "StartButton\($0)" }
+    
+    func startAnimation() {
+        stopAnimation()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { [weak self] _ in
+            guard let self = self else { return }
+            self.currentFrameIndex = (self.currentFrameIndex + 1) % self.animationFrames.count
+        }
+    }
+    
+    func stopAnimation() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
+

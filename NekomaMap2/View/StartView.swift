3ee610dda4a4b24433ddbbacdc2681d9dd4b14d0
nisrinous <nisrinous>
2024-06-25 13:37:57 +0700

    add start view

--- NekomaMap2/View/StartView.swift
+++ NekomaMap2/View/StartView.swift
@@ -13,31 +13,127 @@ struct StartView: View {
     @StateObject private var viewModel = StartViewModel()
     var soundManager = SoundManager()
     
+    @State private var frameIndex = 0
+    private let frames = ["playerWalk0", "playerWalk1", "playerWalk2", "playerWalk3"]
+    
+    func animatePlayer() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            frameIndex = (frameIndex + 1) % frames.count
+            animatePlayer()
+        }
+    }
+    
     var body: some View {
+//        ZStack {
+//            Color.black.ignoresSafeArea()
+//            let currentImage = viewModel.animationFrames[viewModel.currentFrameIndex]
+//            Image(currentImage)
+//                .onTapGesture {
+//                    isLoading = true
+//                    viewModel.stopAnimation()
+//                    soundManager.playSound(fileName: ButtonSFX.start)
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                        soundManager.stopSound(fileName: BGM.homescreen)
+//                        isGameStarted = true
+//                        isLoading = false
+//                    }
+//                }
+//                .onAppear {
+//                    viewModel.startAnimation()
+//                    soundManager.playSound(fileName: BGM.homescreen, loop: true)
+//                }
+//                .onDisappear {
+//                    viewModel.stopAnimation()
+//                    soundManager.stopSound(fileName: BGM.homescreen)
+//                }
+//            
+//        }
+//        .ignoresSafeArea()
+        
         ZStack {
-            Color.black.ignoresSafeArea()
-            let currentImage = viewModel.animationFrames[viewModel.currentFrameIndex]
-            Image(currentImage)
-                .onTapGesture {
+            // Background
+            Color("darkBlue")
+                .edgesIgnoringSafeArea(.all)
+                
+            // Platform
+            VStack(spacing: 0) {
+                Spacer()
+                HStack(spacing: 0) {
+                    // Above Platform Row
+                    ForEach(0..<12) { _ in
+                        Image("abovePlatform")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                }
+                HStack(spacing: 0) {
+                    // Below Platform Row
+                    ForEach(0..<12) { _ in
+                        Image("belowPlatform")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                }
+            }
+            .edgesIgnoringSafeArea(.all)
+
+            // Player
+            VStack {
+                HStack {
+                    GeometryReader { geometry in
+                        Image(frames[frameIndex % frames.count])
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 200)
+                            .foregroundColor(.black)
+                            .padding(.top, 145)
+                            .onAppear {
+                                animatePlayer()
+                            }
+                    }
+                    Image("neki")
+                        .padding(.top, 140)
+                }
+            }
+            
+            // Start Button
+            VStack {
+                Spacer()
+                HStack {
+                    Image("catFace")
+                    VStack {
+                        Image("NEKOMAtext")
+                        Image("THESTORYtext")
+                    }
+                }
+                .padding(.bottom, 40)
+                
+                Button(action: {
                     isLoading = true
-                    viewModel.stopAnimation()
                     soundManager.playSound(fileName: ButtonSFX.start)
                     DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                         soundManager.stopSound(fileName: BGM.homescreen)
-                        isGameStarted = true
+                        isGameStarted.toggle()
                         isLoading = false
                     }
+                }) {
+                    Image("playButtonYellow")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 40)
+                        .shadow(radius: 10)
                 }
                 .onAppear {
-                    viewModel.startAnimation()
                     soundManager.playSound(fileName: BGM.homescreen, loop: true)
                 }
                 .onDisappear {
-                    viewModel.stopAnimation()
                     soundManager.stopSound(fileName: BGM.homescreen)
                 }
-            
+                Spacer()
+            }
+
         }
-        .ignoresSafeArea()
+        
     }
+
 }
